72629c0826b082b2c34485eba4419a3dd33b8b1c
rajankrishnasami <102790130+rajankrishnasami@users.noreply.github.com>
2022-04-02 21:05:17 +0530

    Create basic-training:-add-item-to-an-array.py
    Tags : Fundamentals , Arrays

---
+++ basic-training:-add-item-to-an-array.py
@@ -0,0 +1,12 @@
+# Problem
+
+'''
+Add the value "hello world" to the websites array.
+After your code executes the websites array should == ["hello world"]
+The websites array has already been defined for you using the following code:
+websites = []
+'''
+
+# Solution
+websites = []
+websites.append("hello world")
