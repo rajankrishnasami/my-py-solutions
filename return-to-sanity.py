49a7d3de108e5c48f1a403fad027975f69a5b686
rajankrishnasami <102790130+rajankrishnasami@users.noreply.github.com>
2022-04-03 17:50:20 +0530

    Create return-to-sanity.py
    Tags : Bugs

---
+++ return-to-sanity.py
@@ -0,0 +1,15 @@
+# Problem
+
+'''
+This function should return an object, but it's not doing what's intended. What's wrong?
+def mystery():
+    results = {
+    'sanity': 'Hello'}
+    return
+    results
+'''
+
+# Solution
+def mystery():
+    results = {'sanity': 'Hello'}
+    return results
