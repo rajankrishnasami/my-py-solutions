f17766a56f4f1128da981dbe86b979f2ce4e709d
rajankrishnasami <102790130+rajankrishnasami@users.noreply.github.com>
2022-04-04 22:45:37 +0530

    Create name your python!.py
    Tags : Fundamentals , Classes , Basic Language Features , Object-Oriented-Programming

---
+++ name your python!.py
@@ -0,0 +1,18 @@
+# Problem
+
+'''
+For those of us who are not very familiar with Python, let's handle the very basic challenge of creating a class named Python. We want to give our Pythons a name, so it should take a name argument that we can retrieve later.
+
+For example:
+bubba = Python('Bubba')
+bubba.name # should return 'Bubba'
+'''
+
+# Solution
+
+class Python:
+    def __init__(self,name):
+        self.name = name
+
+bubba = Python('Bubba')
+bubba.name
