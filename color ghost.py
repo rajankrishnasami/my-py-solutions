32091ae47e138c3d6f96c67192911ee1ce9a725a
rajankrishnasami <102790130+rajankrishnasami@users.noreply.github.com>
2022-04-05 22:47:33 +0530

    Create color ghost.py
    Tags : Fundamentals , Object Oriented Programming

---
+++ color ghost.py
@@ -0,0 +1,18 @@
+# Problem
+
+'''
+Create a class Ghost. Ghost objects are instantiated without any arguments.
+Ghost objects are given a random color attribute of white" or "yellow" or "purple" or "red" when instantiated
+
+ghost = Ghost()
+ghost.color  #=> "white" or "yellow" or "purple" or "red"
+'''
+
+# Solution
+
+
+import random
+
+class Ghost(object):
+  def __init__(self):
+    self.color = random.choice(["white", "yellow", "purple", "red"])
